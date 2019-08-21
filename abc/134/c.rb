7a8891ba78105c7e00c93491df56d51764377c45
aocha <ao.hk215@gmail.com>
2019-08-21 23:36:59 +0900

    Update ABC134C - Exception Handling

--- abc/134/c.rb
+++ abc/134/c.rb
@@ -1,27 +1,13 @@
 # 1番目に大きい値が2個以上なら解はすべて1番目に大きくなる
 # 1番目に大きい値が1個しかなければ、解は1番目に大きい値のみ2番目に大きかった値に置き換わる
 N = gets.to_i
-max = { val: 0, cnt: 0 }
-arr = []
+num_seq = []
+N.times { num_seq << gets.to_i }
+sorted_num_seq = num_seq.sort.reverse
 
-N.times do
-  input = gets.to_i
-  arr << input
-
-  if max[:val] == input
-    max[:cnt] += 1
-  elsif max[:val] < input
-    max[:val] = input
-    max[:cnt] = 0
-    max[:cnt] += 1
-  end
+results = num_seq.each_with_object([]) do |num, arr|
+  max = (num == sorted_num_seq[0]) ? sorted_num_seq[1] : sorted_num_seq[0]
+  arr << max
 end
 
-if max[:cnt] > 1
-  puts Array.new(arr.size, max[:val])
-else
-  arr2 = Array.new(arr.size, max[:val])
-  index = arr.index(max[:val])
-  arr2[index] = arr.sort[-2]
-  puts arr2
-end
+puts results

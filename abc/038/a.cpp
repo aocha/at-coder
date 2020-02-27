9581d10ce2b0c7261d5d531764468ea2769d8d43
aocha <ao.hk215@gmail.com>
2020-02-27 23:56:57 +0900

    Add ABC38A - お茶

---
+++ abc/038/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  string S;
+  cin >> S;
+  if (S.at(S.size() - 1) == 'T')
+  {
+    cout << "YES" << endl;
+  }
+  else
+  {
+    cout << "NO" << endl;
+  }
+}

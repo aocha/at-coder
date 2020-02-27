a5a136f762373e07a3f13aab5b3134dd096500d4
aocha <ao.hk215@gmail.com>
2020-02-28 00:01:56 +0900

    Add ABC15A - 高橋くんの研修

---
+++ abc/015/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  string A, B;
+  cin >> A >> B;
+  if (A.size() > B.size())
+  {
+    cout << A << endl;
+  }
+  else
+  {
+    cout << B << endl;
+  }
+}

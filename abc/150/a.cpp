1399f59a50009eda51aaca7bbf8657f9bc963833
aocha <ao.hk215@gmail.com>
2020-06-13 00:17:13 +0900

    Add ABC150A - 500 Yen Coins

---
+++ abc/150/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  int k, x;
+  cin >> k >> x;
+  if (k * 500 >= x)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+}

08e6f618628a72e7991612a11b778ba603595305
aocha <ao.hk215@gmail.com>
2020-06-21 23:34:04 +0900

    Improve ABC171A - alphabet

--- abc/171/a.cpp
+++ abc/171/a.cpp
@@ -3,19 +3,12 @@
 using namespace std;
 typedef long long ll;
 
-char a[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
 int main() {
   char c;
   cin >> c;
-  bool b = false;
-  rep(i, 26) {
-    if (a[i] == c) b = true;
-  }
-  if (b) {
-    cout << 'a' << endl;
-  } else {
+  if (isupper(c)) {
     cout << 'A' << endl;
+  } else {
+    cout << 'a' << endl;
   }
 }
